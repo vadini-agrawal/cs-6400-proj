c9b3ece784b8e3e329d3bb2b7e8af0d54dd162c4
vagrawal48 <vadini@gatech.edu>
2021-10-26 21:17:53 -0400

    Add ability to upload image and save it

--- app/app.py
+++ app/app.py
@@ -1,14 +1,19 @@
-from flask import Flask, render_template, request, redirect, url_for
-from PIL import Image
+import os 
+
+from flask import Flask, render_template, request 
 
 import parse_query
-
 
 # Create Flask's `app` object
 app = Flask(
     __name__,
     template_folder="templates"
 )
+
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+
+def allowed_file(filename):
+	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route("/")
 def hello():
@@ -20,7 +25,25 @@ def sentence_query():
         result = request.form
         result = result['query']
         filenames = ['apple1.jpeg', 'apple2.jpeg', 'apple3.jpeg']
+        # parse_query(result)
         return render_template("index.html",result=result, filenames=filenames)
+
+@app.route("/image-query", methods=['POST'])
+def image_query():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            return "no files"
+        file = request.files['file']
+        if file.filename == '':
+            return "no file"
+        if file and allowed_file(file.filename):
+            filename = file.filename
+            file.save(os.path.join('static', filename))
+            # use saved image to query here
+            return render_template('index.html', filenames=[filename])
+        else:
+            return "File uploaded is not a valid image"
+
 
 if __name__ == "__main__":
     app.jinja_env.auto_reload = True
