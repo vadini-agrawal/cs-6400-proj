2cf9bef6a27227e93b16a0e311825702e813542f
ddebashis3 <shlok@gatech.edu>
2021-11-13 18:49:15 -0500

    Update SQLite databases

--- app/create-db.py
+++ app/create-db.py
@@ -1,15 +1,33 @@
 import sqlite3
-
+import pandas as pd
+import pdb
 # Connect and create cursor
+# conn = sqlite3.connect('image.db')
+# c = conn.cursor()
+# # Create a table 
+# c.execute("""
+#     CREATE TABLE image_features (
+#         id text,
+#         feature text,
+#         url text
+#     )""")
+# conn.commit()
+# conn.close()
 conn = sqlite3.connect('image.db')
-c = conn.cursor()
-
-# Create a table 
-c.execute("""
-    CREATE TABLE image_features (
-        id text,
-        feature text,
-        url text
-    )""")
+df = pd.read_csv("../db/IMAGE.csv")
+df.to_sql("IMAGE", conn, if_exists='replace', index=False)
 conn.commit()
 conn.close()
+
+conn = sqlite3.connect('object.db')
+df = pd.read_csv("../db/OBJECT.csv")
+df.to_sql("OBJECT", conn, if_exists='replace', index=False)
+conn.commit()
+conn.close()
+
+conn = sqlite3.connect('image_object_details.db')
+df = pd.read_csv("../db/IMAGE_OBJECT_DETAILS.csv")
+df.to_sql("IMAGE_OBJECT_DETAILS", conn, if_exists='replace', index=False)
+conn.commit()
+conn.close()
+
